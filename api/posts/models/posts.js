454c4ee9ee11570f3ed44b3e3ed8adff8175f660
Aderci Jr <adercidepaula.jr@gmail.com>
2021-04-08 00:00:49 +0100

    added lifecycle-hooks

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -1,8 +1,19 @@
 'use strict';
+const axios = require('axios')
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  async afterCreate(result, data) {
+    axios.post('https://api.netlify.com/build_hooks/606e38a76ac8c55d87946db4')
+  },
+  async afterUpdate(result, params,  data) {
+    axios.post('https://api.netlify.com/build_hooks/606e38a76ac8c55d87946db4')
+  },
+  async beforeDelete(result, params) {
+    axios.post('https://api.netlify.com/build_hooks/606e38a76ac8c55d87946db4')
+  }
+};
